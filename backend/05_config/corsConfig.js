5ea73c42c6584a138a1f32d18dce9467964f99ad
unknown <ascis2000@gmail.com>
2024-12-02 10:34:54 +0100

    add/files_congif_to_render

--- backend/05_config/corsConfig.js
+++ backend/05_config/corsConfig.js
@@ -1,6 +1,6 @@
 
 const corsOptions = {
-    origin: process.env.NODE_ENV === 'production' ? 'https://mi-dominio.com' : 'http://localhost:5173',
+    origin: process.env.NODE_ENV === 'production' ? 'https://frontend-filmoteca.onrender.com' : 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true
